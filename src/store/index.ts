a8136959e80cd386fbfa14db6446b2c5fb7d4f59
oPablo-Santos <pablo_p9_santos@hotmail.com>
2023-06-01 16:24:03 -0300

    update

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,20 +1,28 @@
 import IProjeto from "@/interfaces/IProjeto";
-import { createStore, Store } from "vuex";
-import { InjectionKey } from 'vue'
+import { createStore, Store, useStore as vuexUseStore} from "vuex";
+import { InjectionKey } from "vue";
 
 interface Estado {
   projetos: IProjeto[];
 }
 
-export const key: InjectionKey<Store<Estado>> = Symbol()
-
+export const key: InjectionKey<Store<Estado>> = Symbol();
 
 export const store = createStore<Estado>({
   state: {
-    projetos: [
-    ],
+    projetos: [],
   },
   mutations: {
-   
-  }
+    'ADICIONA_PROJETOS'(state, nomeDoProjeto: string) {
+      const projeto = {
+        id: new Date().toISOString(),
+        nome: nomeDoProjeto
+      } as IProjeto
+      state.projetos.push(projeto)
+    }
+  },
 });
+
+export function useStore(): Store<Estado> {
+  return vuexUseStore(key)
+}
--- src/views/ProjetosPage.vue
+++ src/views/ProjetosPage.vue
@@ -11,43 +11,46 @@
       </div>
     </form>
     <table class="table is-fullwidth">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nome</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="projeto in projetos" :key="projeto.id">
-                <th>{{ projeto.id }}</th>
-                <th>{{ projeto.nome }}</th>
-            </tr>
-        </tbody>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nome</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="projeto in projetos" :key="projeto.id">
+          <th>{{ projeto.id }}</th>
+          <th>{{ projeto.nome }}</th>
+        </tr>
+      </tbody>
     </table>
   </section>
 </template>
 <script lang="ts">
-import IProjeto from "@/interfaces/IProjeto";
-import { defineComponent } from "vue";
+import { computed, defineComponent } from "vue";
+import { useStore } from "@/store";
 export default defineComponent({
   name: "ProjetosPage",
   data() {
-    return { nomeDoProjeto: "", projetos: [] as IProjeto[] };
+    return { nomeDoProjeto: "" };
   },
   methods: {
     salvar() {
-      const projeto: IProjeto = {
-        nome: this.nomeDoProjeto,
-        id: new Date().toISOString(),
-      };
-      this.projetos.push(projeto);
+      this.store.commit("ADICIONA_PROJETOS", this.nomeDoProjeto);
       this.nomeDoProjeto = "";
     },
+  },
+  setup() {
+    const store = useStore();
+    return {
+      store,
+      projetos: computed(() => store.state.projetos)
+    };
   },
 });
 </script>
 <style scoped>
 .projetos {
-    padding: 1.25rem;
+  padding: 1.25rem;
 }
 </style>
